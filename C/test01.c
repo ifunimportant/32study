89b90d21700cdb0ab78fa9ebf015347aa7fbb679
echoclear <290291207@qq.com>
2026-04-21 11:12:47 +0800

    study printf

--- C/test01.c
+++ C/test01.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+
 int main(void) {
-    printf("hello C\n");
+  system("chcp 936");
+    int ch ;
+    for(ch=30;ch<=100;ch++)
+    {
+printf("ASCII码=%5d，字符=%3c,\t地址验证=%20p,对应十六进制=%x\n",ch,ch,ch,ch,ch);
+    }
+
+
+    printf("你好呀hello C\n");
     return 0;
 }

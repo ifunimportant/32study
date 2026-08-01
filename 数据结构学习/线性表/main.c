f4a5b2935e2a2b9601fd14db093d7ac02cc7243d
ifunimportant <290291207@qq.com>
2026-08-01 17:35:58 +0800

    添加main文件

--- 数据结构学习/线性表/main.c
+++ 数据结构学习/线性表/main.c
@@ -4,6 +4,7 @@
 
 int main()
 {
+    
     //外部定义指针
   array *parray1=getarray();
   arrayinit(parray1);
@@ -13,5 +14,5 @@ int main()
   printf("打印状态： %d",array_writedate(&array2,0,2));
   printf("array1存入的第%d个数是:%d",parray1->last,parray1->sums[parray1->last-1]);
   printf("array2存入的第%d个数是:%d",array2.last,array2.sums[parray1->last-1]);
-  
+
 }
